a4e891e4c34dcce21c21ea3d63d9c7784b5ef96a
azharpr <onesyak19@gmail.com>
2026-05-04 11:30:02 +0700

    Create ambilantrian.php

---
+++ ambilantrian.php
@@ -0,0 +1,45 @@
+<?php
+?>
+<!doctype html>
+<html lang="id">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Horizon Service Center - Pendaftaran</title>
+    <link rel="stylesheet" href="ambilantrian.css" />
+  </head>
+
+  <body>
+    <div class="sidebar">
+      <div class="logo-container">
+        <img src="logo.png" />
+      </div>
+
+      <ul class="nav-links">
+        <li><a href="ambilantrian.html">Ambil Antrian</a></li>
+        <li><a href="daftarantrian.html">Daftar Antrian</a></li>
+        <li><a href="kartuantrian.html">Kartu Antrian</a></li>
+      </ul>
+
+      <div class="sidebar-footer"></div>
+    </div>
+
+    <div class="main-content">
+      <div class="header">PENDAFTARAN PELANGGAN</div>
+
+      <div class="form-container">
+        <div class="card">
+          <div class="input-group">
+            <input type="text" placeholder="No. Telepon" />
+          </div>
+
+          <div class="input-group">
+            <input type="date" />
+          </div>
+
+          <button class="submit-btn">SUBMIT</button>
+        </div>
+      </div>
+    </div>
+  </body>
+</html>
